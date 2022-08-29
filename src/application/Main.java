dd3642a1f1b360385c414dfe126c528fafe005f7
Fulano de Tal <felipesand824@gmail.com>
2022-08-29 16:45:20 -0300

    Seller entity class

---
+++ src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Date;
+
+import model.entites.Department;
+import model.entites.Seller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
+		Seller obj2 = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(obj2);
+
+	}
+
+}
